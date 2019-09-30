63f99070ee1077221b282a97a51312e8d2f490b7
Ken Crowell <oeuftete@gmail.com>
2019-09-30 11:30:19 -0300

    Start framing out the tool

---
+++ generate_daily_entry.py
@@ -0,0 +1,24 @@
+def main():
+    """
+    Generate the daily blog entry for a given day.
+    """
+
+    #  Drive file generation
+    #
+    #  TODO: What date?  Pass by YYYYMMDD arg or default to current in Atlantic.
+    #  TODO: Get the template file for the day of the week.
+    #  TODO: Template files are by convention or just configurable IDs?
+    #  TODO: Copy that to a file named conventionally, e.g. "Monday, 9/30/19"
+    #  TODO: The template file may be restricted in permissions, but ensure the
+    #  daily file is open.
+    #  TODO: Don't create the daily file if it already exists.
+    #  TODO: Get a link for the daily file either way to add to the blog entry.
+
+    #  Blog entry generation
+    #  TODO: Generate the post if it doesn't exist.
+    #  TODO: Post content is "Add your times here." anchor linked to the
+    #  spreadsheet for the day.
+
+
+if __name__ == '__main__':
+    main()
